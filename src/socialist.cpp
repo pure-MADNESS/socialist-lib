f4061e59d6495b8640dbed8514664c5c11bdfdc7
alterlleo <leonardo.pasquato@gmail.com>
2026-03-17 15:35:01 +0100

    feat: setup listen function
    populate _neighbours and _powers

--- src/socialist.cpp
+++ src/socialist.cpp
@@ -1,1 +1,79 @@
+/*
+
+  ____             _       _ _     _          _               
+ / ___|  ___   ___(_) __ _| (_)___| |_    ___| | __ _ ___ ___ 
+ \___ \ / _ \ / __| |/ _` | | / __| __|  / __| |/ _` / __/ __|
+  ___) | (_) | (__| | (_| | | \__ \ |_  | (__| | (_| \__ \__ \
+ |____/ \___/ \___|_|\__,_|_|_|___/\__|  \___|_|\__,_|___/___/
+                                                              
+
+*/
+
 #include "socialist.hpp"
+
+void Socialist::listen_sources(json const &input, string topic){
+
+// if source node
+  if(topic.rfind("source", 0) == 0){
+
+    auto iter_sources = _powers.find(topic);
+    if(input.contains("hourly")){
+      
+      auto const& json_array = input.at("hourly");
+
+      if(iter_sources != _powers.end()){
+        for(int i = 0; i < HOURS; ++i){
+          iter_sources -> second._powers[i] = json_array.at(i).get<double>();
+        }
+      } else {
+
+        FuturePowers new_hourly;
+
+        for(size_t i = 0; i < HOURS; ++i){
+          new_hourly._powers[i] = json_array.at(i).get<double>();
+        }
+        _powers[topic] = new_hourly;
+      }
+    }
+
+  // if load node
+  } else if(topic.rfind("load", 0) == 0){
+
+    auto iter_loads = _neighbours.find(topic);
+    auto now = steady_clock::now();
+
+    if(iter_loads != _neighbours.end() && input.contains("hourly")){
+
+      auto const& json_array = input.at("hourly");
+
+      for(int i = 0; i < HOURS; i++){
+        iter_loads -> second._requests[i] = json_array.at("requests").at(i).get<double>();
+        iter_loads -> second._flex[i] = json_array.at("flexibilities").at(i).get<double>();
+      }
+      
+    } else{
+
+      Strategy new_load;
+
+      auto const& json_array = input.at("hourly");
+      
+      for(int i = 0; i < HOURS; i++){
+        new_load._requests[i] = json_array.at("requests").at(i).get<double>();
+        new_load._flex[i] = json_array.at("flexibilities").at(i).get<double>();
+      }
+    
+      _neighbours[topic] = new_load;
+    }
+
+  } else{
+
+    return;
+  }
+  
+}
+
+void update_strategy(){
+
+
+    
+}
--- src/socialist.hpp
+++ src/socialist.hpp
@@ -12,11 +12,59 @@
 #ifndef __SOCIALIST_H__
 #define __SOCIALIST_H__
 
+#include <string.h>
+#include <iostream>
+#include <vector>
+#include <array>
+#include <chrono>
+#include <nlohmann/json.hpp>
+
+#define HOURS 24
+
+using namespace std;
+using json = nlohmann::json;
+using namespace chrono;
+
+struct Strategy{
+
+  vector<double> _requests;
+  vector<double> _flex;
+  steady_clock::time_point _last_active; 
+};
+
+struct FuturePowers{
+
+  vector<double> _powers;
+  steady_clock::time_point _last_active; 
+};
+
 class Socialist{
 
+  public:
+    Socialist();
+    ~Socialist();
+
+    void listen_sources(json const &input, string topic);
+
+    /**
+     * 
+     * - residuals, for the negative ones check if this -> _strategy is the one with the lowest flexibilty for the given hour. If it is, shift it
+     * 
+     */
+    void update_strategy();
 
 
-}
+  private:
+  
+    Strategy _strategy;
+
+    vector<double> _tot_powers; // somma delle potenze per ogni ora
+    vector<double> _residuals; // margini
+    map<string, Strategy> _neighbours; // [load_1, strategy(due vettori)] [load_2, strategy] [load_3..]...
+    // _neightbors[1] -> first
+    // _neighbours[1] -> second._requests // _flex
+    map<string, FuturePowers> _powers;
+};
 
 
 #endif
